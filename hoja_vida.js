a16cf52b5cfcce621cfab9ad24cd2f83b6636e77
JuanQuitian123 <60011329+JuanQuitian123@users.noreply.github.com>
2020-05-18 23:17:01 -0500

    Update hoja_vida.js

--- hoja_vida.js
+++ hoja_vida.js
@@ -1,9 +1,17 @@
 const express = require('express'),
     app = express(),
+    cookieParser = require('cookie-parser'),
+    cookieSession = require('cookie-session');
     path = require('path'); 
+
     app.use(express.static(path.join(__dirname, "/public")));
+    app.use(cookieParser());
+    app.use(cookieSession({secret:"I´m a secret"}));
 
     app.get('/',(peticion,respuesta)=>{
+        peticion.session.visitas || (peticion.session.visitas = 0);
+        let n = peticion.session.visitas++
+        console.log(`Visita # ${n}`);
         respuesta.sendFile(`${__dirname}/views/index.html`);
     });
 
@@ -17,23 +25,6 @@ const express = require('express'),
             respuesta.sendFile(`${__dirname}/views/aptitudes.html`);
         }); 
 
-
-
-
-    cookieParser = require('cookie-parser'),
-    cookieSession = require('cookie-session');
-
-    // USo de MIDDLEWARES
-    app.use(cookieParser());
-    app.use(cookieSession({secret:"I´m a secret"}));
-
-
-    app.get('/', (peticion,respuesta)=>{
-
-        peticion.session.visitas || (peticion.session.visitas = 0);
-        let n = peticion.session.visitas++
-        respuesta.end(`<h1>Visita # ${n}</h1>`);
-    });
 
     app.listen(8080);
     console.log('Conexión realizada');
